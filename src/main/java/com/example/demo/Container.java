042346bc63c018dc862f16b87fe5af147409e3af
Ramsundar S <87656738+ramsundar07@users.noreply.github.com>
2023-12-27 12:47:48 +0530

    Update Container.java

--- src/main/java/com/example/demo/Container.java
+++ src/main/java/com/example/demo/Container.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class Container {
