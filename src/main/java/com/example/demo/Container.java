ddaba20d45add32cfb3190a36e0fe022e0f3c1b1
Ramsundar S <87656738+ramsundar07@users.noreply.github.com>
2023-12-27 12:34:04 +0530

    Update Container.java

--- src/main/java/com/example/demo/Container.java
+++ src/main/java/com/example/demo/Container.java
@@ -3,11 +3,11 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@RestController
 public class Container {
 	@RequestMapping("/")
 	public String home() {
 		System.out.print("KHGJHGJHFGJHG");
-		return "home.jsp";
+		return "hi";
 	}
 }
