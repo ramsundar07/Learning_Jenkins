1c0bc605e515076b4b90ae4d1ae05ddd138e5bc0
Ramsundar S <87656738+ramsundar07@users.noreply.github.com>
2023-12-27 12:46:10 +0530

    Update Container.java

--- src/main/java/com/example/demo/Container.java
+++ src/main/java/com/example/demo/Container.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class Container {
-	@RequestMapping("/")
+	@GetMapping("/home")
 	public String home() {
 		System.out.print("KHGJHGJHFGJHG");
 		return "hi";
